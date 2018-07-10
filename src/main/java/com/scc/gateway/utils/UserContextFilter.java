465b8ab1b009c5fb583883eda9f1b071ea07f48e
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-07-10 16:13:13 +0200

    last update

--- src/main/java/com/scc/gateway/utils/UserContextFilter.java
+++ src/main/java/com/scc/gateway/utils/UserContextFilter.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import io.opentracing.Scope;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -33,11 +31,6 @@ public class UserContextFilter implements Filter {
 		UserContextHolder.getContext()
 				.setAuthentificationKey(httpServletRequest.getHeader(UserContext.AUTHENTICATION_KEY));
 		logger.debug("Incoming Authentification key: {}", UserContextHolder.getContext().getAuthentificationKey());
-
-		Scope scope = tracer.scopeManager().active();
-		if (scope != null) {
-		    scope.span().log("Ici");
-		}
 
 		filterChain.doFilter(httpServletRequest, servletResponse);
 	}
